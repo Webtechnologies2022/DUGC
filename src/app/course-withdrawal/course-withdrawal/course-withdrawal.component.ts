75a581c7e017a68a106390a48aafd70e69f37124
Prajwal Patil <prajwalpatilk@gmail.com>
2022-12-02 20:14:41 +0530

    Update status message with toastr

--- src/app/course-withdrawal/course-withdrawal/course-withdrawal.component.ts
+++ src/app/course-withdrawal/course-withdrawal/course-withdrawal.component.ts
@@ -2,56 +2,61 @@ import { Component, OnInit } from '@angular/core';
 import { StudentsService } from '../services/students.service';
 import { HttpClient } from '@angular/common/http';
 import { Int32 } from 'mongodb';
-
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-course-withdrawal',
   templateUrl: './course-withdrawal.component.html',
-  styleUrls: ['./course-withdrawal.component.css']
-
+  styleUrls: ['./course-withdrawal.component.css'],
 })
 export class CourseWithdrawalComponent implements OnInit {
-  students:any;
-  sno:number = 1
-  constructor(private httpClient: HttpClient, private studentService: StudentsService) { }
+  students: any;
+  sno: number = 1;
+  constructor(
+    private httpClient: HttpClient,
+    private studentService: StudentsService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
-    this.getAllStudents()
+    this.getAllStudents();
   }
 
-  getAllStudents(){
+  getAllStudents() {
     // this.httpClient.get(this.url).subscribe(response => {
     //   this.students = response
     // })
-    this.studentService.getAllStudents().subscribe((data:any)=>{
+    this.studentService.getAllStudents().subscribe((data: any) => {
       this.students = data;
       // console.log(this.students)
       // this.students = this.students;
       // console.log(this.students[0].selectedCourseToWithdraw[0].name);
-    })
+    });
   }
-  approve(id:any){
-    this.studentService.updateStudent(id, {status:'approved'}).subscribe((data)=>{
-      console.log(data)
-      alert('Approved')
-    this.getAllStudents()
-
-    })
-  }
-  
-  reject(id:any){
-    this.studentService.updateStudent(id, {status:'rejected'}).subscribe((data)=>{
-      console.log(data)
-      alert('Rejected')
-    this.getAllStudents()
-
-    })
+  approve(id: any) {
+    this.studentService
+      .updateStudent(id, { status: 'approved' })
+      .subscribe((data) => {
+        console.log(data);
+        this.toastr.success('Operation successful', 'Approved');
+        this.getAllStudents();
+      });
   }
 
-  incrementSno(){
-    this.sno+=1;
+  reject(id: any) {
+    this.studentService
+      .updateStudent(id, { status: 'rejected' })
+      .subscribe((data) => {
+        console.log(data);
+        this.toastr.success('Operation successful', 'Rejected');
+        this.getAllStudents();
+      });
   }
 
-  resetSno(){
+  incrementSno() {
+    this.sno += 1;
+  }
+
+  resetSno() {
     this.sno = 1;
   }
 }
