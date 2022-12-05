b746e541ff9c3da92d3b942807c8dbae65c684c6
jithu <jithuwolf789@gmail.com>
2022-12-05 14:03:09 +0530

    asset images added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  BASE_URL:'http://localhost:3000',
+    STUDENT_BASE_URL:'http://localhost:3000/student/',
+    STUDENT_LAB_LIST:'http://localhost:3000/lab/',
+    STUDENT:{
+        ADD_STUDENT:'addmany',
+        GET_ALL_STUDENTS:'list',
+        UPDATE_STUDENTS:'update',
+        DELETE_STUDENT:'delete',
+        MARKS:'marks',
+        ATTENDANCE:'attendance'
+    }
 };
