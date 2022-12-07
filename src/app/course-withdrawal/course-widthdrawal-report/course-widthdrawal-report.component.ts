b333e196f4a278f5bb3b0181ea08d817891e61d3
Sandeep Urankar <sandeepurankar.98@gmail.com>
2022-12-07 07:10:11 +0530

    Modified course-withdraw module, add consolidated table.

--- src/app/course-withdrawal/course-widthdrawal-report/course-widthdrawal-report.component.ts
+++ src/app/course-withdrawal/course-widthdrawal-report/course-widthdrawal-report.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { StudentsService } from '../services/students.service';
+import { combineLatest } from 'rxjs';
 @Component({
   selector: 'app-course-widthdrawal-report',
   templateUrl: './course-widthdrawal-report.component.html',
@@ -29,9 +30,48 @@ export class CourseWidthdrawalReportComponent implements OnInit {
   resetSno(){
     this.sno = 1;
   }
-
+  stud:any
+  studentsOfSelectedSem:any = []
+  courseCode:any = new Set()
+  courseName:any = new Set()
+  i = 0
+  c:number = 0
   getSemReport(selectedSem:string){
+    this.courseCode = new Set()
+    this.courseName = new Set()
     console.log(selectedSem)
     this.selectedSem = selectedSem
+    for(this.stud of this.students){
+      if(this.stud.sem == selectedSem){
+        console.log(this.stud)
+        this.studentsOfSelectedSem.push(this.stud)
+        for(this.c=0; this.c < this.stud.selectedCourseToWithdraw.length; this.c++){
+          this.courseCode.add(this.stud.selectedCourseToWithdraw[this.c].code)
+          this.courseName.add(this.stud.selectedCourseToWithdraw[this.c].name)
+        }
+      }
+      this.i++
+    }
+    this.courseCode =  Array.from(this.courseCode)
+    this.courseName =  Array.from(this.courseName)
+    
+    console.log(this.courseCode)
+    console.log(this.courseName)
+  
+
+    this.i = 0
   }
+
+  startSpaces = 0
+  trailSpaces = 0
+  getPosition(course:any){
+    this.startSpaces =this.courseCode.indexOf(course.code)
+    // console.log("startSpace",this.startSpaces)
+
+    this.trailSpaces = this.courseCode.length - this.startSpaces - 1
+  }
+
+  counter(i: number) {
+    return new Array(i);
+}
 }
