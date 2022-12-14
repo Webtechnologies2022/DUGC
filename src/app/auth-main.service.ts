79017227b8f05ecccdf80f689e73d5cbe7939530
Prajwal Patil <prajwalpatilk@gmail.com>
2022-12-14 22:50:15 +0530

    Change port to 3000

--- src/app/auth-main.service.ts
+++ src/app/auth-main.service.ts
@@ -1,64 +1,56 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {BehaviorSubject} from 'rxjs'
-import  { JwtHelperService} from '@auth0/angular-jwt'
+import { BehaviorSubject } from 'rxjs';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthMainService {
-  jwthelp =new JwtHelperService;
-  userCredentials:BehaviorSubject<any>=new BehaviorSubject(null);
-  private  Loginurl = 'http://localhost:3003/login/admin';
-  private  Loginurl2= 'http://localhost:3003/login/admin';
-  private  Loginurl3 = 'http://localhost:3003/login/admin';
-  private  registerurl = 'http://localhost:3003/login/add';
-  constructor(private http:HttpClient,private route:Router) {
+  jwthelp = new JwtHelperService();
+  userCredentials: BehaviorSubject<any> = new BehaviorSubject(null);
+  private Loginurl = 'http://localhost:3000/login/admin';
+  private Loginurl2 = 'http://localhost:3000/login/admin';
+  private Loginurl3 = 'http://localhost:3000/login/admin';
+  private registerurl = 'http://localhost:3000/login/add';
+  constructor(private http: HttpClient, private route: Router) {}
 
-   }
-
-  LoginUser(user:any)
-  {
-  return this.http.post(this.Loginurl,user) 
+  LoginUser(user: any) {
+    return this.http.post(this.Loginurl, user);
   }
 
-  RegisterUser(user:any)
-  {
-    return this.http.post(this.registerurl,user) 
+  RegisterUser(user: any) {
+    return this.http.post(this.registerurl, user);
   }
 
-  LoggedIn()
-  {
+  LoggedIn() {
     return !!localStorage.getItem('token');
   }
 
-  gettoken()
-  {
-   return localStorage.getItem('token');
+  gettoken() {
+    return localStorage.getItem('token');
   }
-  logout()
-  {
+  logout() {
     localStorage.removeItem('token');
     this.route.navigate(['/']);
-  //   console.log(user);
-  //   const accesstoken ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlRlc3QiLCJzdWIiOjIsImlhdCI6MTYwNDMwOTc0OSwiZXhwIjoxNjA0MzA5ODA5fQ.jHez9kegJ7GT1AO5A2fQp6Dg9A6PBmeiDW1YPaCQoYs";
-  //   const refreshtoken = "dummy";
+    //   console.log(user);
+    //   const accesstoken ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlRlc3QiLCJzdWIiOjIsImlhdCI6MTYwNDMwOTc0OSwiZXhwIjoxNjA0MzA5ODA5fQ.jHez9kegJ7GT1AO5A2fQp6Dg9A6PBmeiDW1YPaCQoYs";
+    //   const refreshtoken = "dummy";
 
-  //   localStorage.setItem("tkn", accesstoken);
-  //   localStorage.setItem("rtkn", refreshtoken);
-  //   // return this.http.post(this.url+'/user/add-user', user);
-  //   const decrrpt= this.jwthelp.decodeToken(accesstoken);
-  //   console.log(decrrpt);
-  //     const data = 
-  // {
-  //   access:accesstoken,
-  //   refresh:refreshtoken,
-  //   username:decrrpt.username,
-  //   userid:decrrpt.sub,
-  //   exp:decrrpt.exp
-  // }
-  // this.userCredentials.next(data);
-  
+    //   localStorage.setItem("tkn", accesstoken);
+    //   localStorage.setItem("rtkn", refreshtoken);
+    //   // return this.http.post(this.url+'/user/add-user', user);
+    //   const decrrpt= this.jwthelp.decodeToken(accesstoken);
+    //   console.log(decrrpt);
+    //     const data =
+    // {
+    //   access:accesstoken,
+    //   refresh:refreshtoken,
+    //   username:decrrpt.username,
+    //   userid:decrrpt.sub,
+    //   exp:decrrpt.exp
+    // }
+    // this.userCredentials.next(data);
   }
 }
--- src/app/course-withdrawal/services/students.service.ts
+++ src/app/course-withdrawal/services/students.service.ts
@@ -2,26 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StudentsService {
-  private url = 'http://localhost:3003/withdrawal';
-  constructor(private httpClient: HttpClient) { }
+  private url = 'http://localhost:3000/withdrawal';
+  constructor(private httpClient: HttpClient) {}
 
-  getAllStudents(){
+  getAllStudents() {
     // console.log(this.httpClient.get(this.url+'/'))
-    return this.httpClient.get(this.url+'/getStudents');
+    return this.httpClient.get(this.url + '/getStudents');
   }
 
-
-  insertStudent(details:any){
-    return this.httpClient.post(this.url+'/add-student', details);
+  insertStudent(details: any) {
+    return this.httpClient.post(this.url + '/add-student', details);
   }
-  
-  updateStudent(user:any, status:any){
-    console.log(status)
-    console.log(user)
-    console.log(this.url+'/update-student/'+user)
-    return this.httpClient.put(this.url+'/update-student/'+user, status);
+
+  updateStudent(user: any, status: any) {
+    console.log(status);
+    console.log(user);
+    console.log(this.url + '/update-student/' + user);
+    return this.httpClient.put(this.url + '/update-student/' + user, status);
   }
 }
