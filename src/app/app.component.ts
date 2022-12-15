3b5f1289bac62f10249f3a7f526a4e68457998ad
Prajwal Patil <prajwalpatilk@gmail.com>
2022-12-15 10:39:56 +0530

    Add welcome message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,18 @@ import { AuthMainService } from './auth-main.service';
 export class AppComponent {
   title = 'DUGC';
   user_type: any = '';
+  user_type_name: any = '';
   constructor(private auth: AuthMainService) {}
 
   log() {
+    this.user_type = this.auth.getUserType();
+    if (this.user_type == 'CourseCoordinator') {
+      this.user_type_name = 'Course Coordinator';
+    } else if (this.user_type == 'DUGCCoordinator') {
+      this.user_type_name = 'DUGC Coordinator';
+    } else if (this.user_type == 'DUGCChairman') {
+      this.user_type_name = 'DUGC Chairman';
+    }
     if (this.auth.LoggedIn()) {
       return this.auth.LoggedIn();
     } else if (this.auth.dugc()) {
@@ -21,9 +30,18 @@ export class AppComponent {
     }
   }
   logout() {
+    this.user_type = '';
+    this.user_type_name = '';
     return this.auth.logout();
   }
   ngOnInit(): void {
     this.user_type = this.auth.getUserType();
+    if (this.user_type == 'CourseCoordinator') {
+      this.user_type_name = 'Course Coordinator';
+    } else if (this.user_type == 'DUGCCoordinator') {
+      this.user_type_name = 'DUGC Coordinator';
+    } else if (this.user_type == 'DUGCChairman') {
+      this.user_type_name = 'DUGC Chairman';
+    }
   }
 }
